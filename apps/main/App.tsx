20a7c8b770f938141f4e74337bf3232b432ab8e6
teramotodaiki <i101323@gmail.com>
2025-08-11 19:15:25 +0900

    hello world

---
+++ apps/main/App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text accessibilityRole="header" style={styles.text}>Hello, world</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: '#111'
+  }
+});
